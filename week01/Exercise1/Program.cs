60c303bf1dba9f77b40f11db5c829f7105831559
charissa-code <cmaes@byupathway.edu>
2026-05-05 16:15:32 -0500

    Added a comment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,6 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Asked user for their name.
         Console.Write("What is your first name? ");
         string fname = Console.ReadLine();
 
