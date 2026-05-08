cc1a6954b7bfe1cd3ed8febe188d38ee8eb6491a
charissa-code <cmaes@byupathway.edu>
2026-05-08 17:39:20 -0500

    initial push

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.WriteLine("Write a number. Enter '0' when you are finished.");
+            string userNumber = Console.ReadLine();
+            number = int.Parse(userNumber);
+            if (userNumber != "0")
+                numbers.Add(number);
+        }
+
+        Console.WriteLine($"The total of all numbers is {numbers.Sum()}.");
+        Console.WriteLine($"The average of all numbers is {numbers.Average()}");
+        Console.WriteLine($"The largest number is {numbers.Max()}");
+
+        int smallestPos = numbers.Where(n => n > 0).Min();  //.Where goes through every item in numbers and returns on the ones "where" the condition is true. (n => n > 0) is called a lambda expression. "Take each number n and check if n>0." if true-keep  if false- ignore it. Like a sieve that removes items you don't want.
+        Console.WriteLine($"The smallest positive number is {smallestPos}.");
+          
+
     }
 }
